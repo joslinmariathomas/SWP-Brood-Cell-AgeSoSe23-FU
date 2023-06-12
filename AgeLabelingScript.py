15d529bb12ab33a2d4b37d348e6d288e1983bab7
runnninggag <lukas_ludwig@gmx.net>
2023-06-12 02:09:10 +0200

    Improved age labeling: account for scanner fails, check if stages are valid, adjust values, a lot of testing; Plots now have an (optional) age graph

--- AgeLabelingScript.py
+++ AgeLabelingScript.py
@@ -8,31 +8,57 @@ output_data = []
 new_egg_timestamps = np.full((18,16), -1.0)
 prev_states = np.full((18,16), '(empty)', dtype='object')
 
-def get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back, thresh_ahead):
+def is_stage_valid(stage_label, prev_frame_age):
+    match stage_label:
+        case '(has egg)':
+            # <= 6 days is valid
+            return prev_frame_age <= 518400
+        case '(has larva)':
+            # >= 1 days and <= 11 days is valid
+            return 86400 <= prev_frame_age <= 950400
+        case '(has young pupa)' | '(has old pupa)':
+            # >= 6 days is valid
+            return 518400 <= prev_frame_age
+        case '(has bee head)':
+            # >= 9 days is valid
+            return 777600 <= prev_frame_age
+        case _:
+            return True
+
+def get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back, thresh_ahead, use_for_trans_to_empty = True):
+    # if scanner failed before current frame but still in the search interval, then look only back to the last frame after the fail but further ahead (so that interval stays the same)
+    if use_for_trans_to_empty:
+        min_frame_id = frame_ind + thresh_back
+        # scanner failed 3 times, on frame-IDs: 1056 -> 1057 and 1819 -> 1820 and 2066 -> 2067
+        if min_frame_id < 1056 < frame_ind:
+            diff = 1056 - min_frame_id
+            # take 10 more frames before scanner fail in case only 'empty' follows
+            thresh_back -= diff + 10
+            thresh_ahead += diff
+        elif min_frame_id < 1819 < frame_ind:
+            diff = 1819 - min_frame_id
+            # take 10 more frames before scanner fail in case only 'empty' follows
+            thresh_back -= diff + 10
+            thresh_ahead += diff
+
     # check surrounding labels and make a list of labels (excluding unknown)
     labels = []
     for i in range(thresh_back, thresh_ahead+1):
         if frame_ind+i < len(frames) \
             and frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label'] != '(unknown)' \
-            and not ',' in frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label']:
+            and not ',' in frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label'] \
+            and frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label'] != '()':
             labels.append(frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label'])
     return Counter(labels)
 
 def iterate_frames(frames):
-    num_frames = len(frames)
-    print(len(frames))
     trans_empty_to_egg = 0
     num_wrong_trans_to_empty = 0
     num_wrong_trans_to_egg = 0
-    #thresh_ahead = 100
-    # when we look more ahead at the labels to find out which state likely comes next
-    thresh_back_forward = -120
-    thresh_ahead_forward = 144
-    # when we look more backwards at the labels to find the most likely state we are currently in 
-    thresh_back_backward = -100
-    thresh_ahead_backward = 10
+    
     list_frame_ind = []
-    for frame_ind in range(num_frames):
+    # after frame 2066 there is a 29 day gap and then only a few more labels that are mostly bad, so cut them off
+    for frame_ind in range(2067):
         frame_dict = frames[frame_ind]
         # list of cells for this frame
         cells = frame_dict['cells']
@@ -40,6 +66,13 @@ def iterate_frames(frames):
 
         for row_ind, cell_row in enumerate(cells):
             for col_ind, cell in enumerate(cell_row):
+                # when we look at the surrounding labels at the START of a lifecycle to find out which state likely comes next
+                thresh_back_start = -120
+                thresh_ahead_start = 144
+                # when we look at the surrounding labels to find the most likely state we are currently in
+                thresh_back_current = -100
+                thresh_ahead_current = 50
+
                 prev_states[row_ind][col_ind]
 
                 wrong_trans = False
@@ -52,18 +85,11 @@ def iterate_frames(frames):
                     age = 0
                     # transition from empty to egg: cell was previously labeled empty and now its label contains 'egg'
                     if 'egg' in label:
-                        cnt = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_forward, thresh_ahead_forward)
+                        cnt = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_start, thresh_ahead_start)
                         # if more than 70% are empty or less than 25% have egg
                         if cnt['(empty)'] > np.round(cnt.total() * 0.7) \
                             or cnt['(has egg)'] < np.round(cnt.total() * 0.25):
                             wrong_trans = True
-
-                        """
-                        for i in range(1,thresh_ahead):
-                            if frame_ind+i < num_frames and not 'egg' in frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label']:
-                                wrong_trans = True
-                                break
-                        """
                         if wrong_trans:
                             num_wrong_trans_to_egg += 1
                         else:
@@ -79,59 +105,85 @@ def iterate_frames(frames):
                     age = time - new_egg_timestamps[row_ind][col_ind]
                 # transition from not empty to empty
                 else:
-                    cnt_backward = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_backward, thresh_ahead_backward)
-                    cnt = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_forward, thresh_ahead_forward)
+                    # use this counter just to guess the current state
+                    cnt_current = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_current, thresh_ahead_current, use_for_trans_to_empty=False)
+
                     # get label with highest count to get most likely current state (if empty is highest then take next highest one)
                     # maybe also work with time for stage thresholds?
-                    most_common_current = cnt_backward.most_common()
+                    most_common_current = cnt_current.most_common()
                     i = 0
-                    while len(most_common_current) > i and most_common_current[i][0] == '(empty)':
+                    while len(most_common_current) > i and \
+                        (most_common_current[i][0] == '(empty)' \
+                         # check if this stage conforms with the age
+                         or not is_stage_valid(most_common_current[i][0], frames[frame_ind - 1]['cells'][row_ind][col_ind]['age'])):
                         i+=1
-                    if len(most_common_current) > i:
+
+                    current_state = '(empty)'
+                    if i < len(most_common_current):
                         current_state = most_common_current[i][0]
-                        match current_state:
-                            case '(has egg)':
-                                min_empty = np.round(cnt.total() * 0.8)
-                                max_current = np.round(cnt.total() * 0.55)
-                            case '(has larva)':
-                                min_empty = np.round(cnt.total() * 0.85)
-                                max_current = np.round(cnt.total() * 0.35)
-                            case '(has young pupa)' | '(has old pupa)':
-                                min_empty = np.round(cnt.total() * 0.5)
-                                max_current = np.round(cnt.total() * 0.8)
-                            case '(has bee head)':
-                                min_empty = np.round(cnt.total() * 0.85)
-                                max_current = np.round(cnt.total() * 0.4)
-                            case _:
-                                wrong_trans = True
-                    else:
-                        wrong_trans = True
+
+                    # look further ahead, because it is unlikely that the cell gets cannibalized and thus it is likely the bee makes it to adulthood
+                    if current_state == '(has young pupa)' or current_state == '(has old pupa)':
+                        thresh_ahead_start = 200
+                        thresh_back_start = -80
+                    # additionally look less back because for the last stage this doesn't interest us as much anymore
+                    elif current_state == '(has bee head)':
+                        thresh_ahead_start = 400
+                        thresh_back_start = -20
+                    
+                    # use this counter for the actual transition
+                    cnt = get_label_counter(row_ind, col_ind, frame_ind, frames, thresh_back_start, thresh_ahead_start)
+
+                    match current_state:
+                        case '(has egg)':
+                            min_empty = np.round(cnt.total() * 0.85)
+                            max_current_or_later = np.round(cnt.total() * 0.15)
+                            # we have to look at later stages too because of periods of missing data (example is cell 10, 9)
+                            actual_current_or_later = cnt[current_state] + cnt['(has larva)'] + cnt['(has young pupa)'] + cnt['(has old pupa)'] + cnt['(has bee head)']
+                        case '(has larva)':
+                            min_empty = np.round(cnt.total() * 0.8)
+                            max_current_or_later = np.round(cnt.total() * 0.2)
+                            actual_current_or_later = cnt[current_state] + cnt['(has young pupa)'] + cnt['(has old pupa)'] + cnt['(has bee head)']
+                        case '(has young pupa)':
+                            min_empty = np.round(cnt.total() * 0.95)
+                            max_current_or_later = np.round(cnt.total() * 0.3)
+                            actual_current_or_later = cnt[current_state] + cnt['(has old pupa)'] + cnt['(has bee head)']
+                        case '(has old pupa)':
+                            min_empty = np.round(cnt.total() * 0.95)
+                            max_current_or_later = np.round(cnt.total() * 0.3)
+                            # here we also use has young pupa (although it's an earlier stage) because both are frequently confused
+                            actual_current_or_later = cnt[current_state] + cnt['(has young pupa)'] + cnt['(has bee head)']
+                        case '(has bee head)':
+                            min_empty = np.round(cnt.total() * 0.9)
+                            # if a few bee heads are present then don't transition
+                            max_current_or_later = np.round(cnt.total() * 0.025)
+                            actual_current_or_later = cnt[current_state]
+                        case _:
+                            wrong_trans = True
+                            actual_current_or_later = -1
 
                     actual_empty = cnt['(empty)']
-                    actual_current_state = cnt[current_state]
-                    if row_ind == 7 and col_ind == 4 and frame_ind == 553:
+                    if row_ind == 8 and col_ind == 7 and frame_ind == 1897:
+                        print(cnt_current.most_common())
                         print(cnt.most_common())
                         print(f'current_state: {current_state}, \n \
                             min_empty: {min_empty}, \n \
-                            max_current: {max_current}, \n \
+                            max_current_or_later: {max_current_or_later}, \n \
                             actual empty: {actual_empty}, \n \
-                            actual_current_state: {actual_current_state}')
+                            actual_current_or_later: {actual_current_or_later}')
                     # if less than min_empty cells are labeled 'empty' or more than max_current ...
                     #   ... are labeled the same as the current state the transition is likely wrong
                     if not wrong_trans \
                         and (actual_empty < min_empty \
-                        or actual_current_state > max_current):
+                        or actual_current_or_later > max_current_or_later):
                         wrong_trans = True
-                    # adjust how many 'empty' labels are necessary to transition to empty based on current state
-                    """
-                    for i in range(1,thresh_ahead):
-                        if frame_ind+i < num_frames and frames[frame_ind + i]['cells'][row_ind][col_ind]['pred_label'] != '(empty)':
-                            wrong_trans = True
-                            break
-                    """
                     if wrong_trans:
                         num_wrong_trans_to_empty += 1
                         age = time - new_egg_timestamps[row_ind][col_ind]
+                        # if age > 24 days force transition
+                        if age > 2073600:
+                            age = 0
+                            wrong_trans = False
                     else:
                         age = 0
 
@@ -155,15 +207,6 @@ def iterate_frames(frames):
     print(f'number of wrong transitions to egg: {num_wrong_trans_to_egg}')
     print(f'number of wrong transitions to empty: {num_wrong_trans_to_empty}')
     print(list_frame_ind)
-    for frame_ind in range(0,630):
-        print(frames[frame_ind]['cells'][11][14])
-    print("\n")
-    for frame_ind in range(1070,1110):
-        print(frames[frame_ind]['cells'][0][12])
-    print("\n")
-    for frame_ind in range(1900,2060):
-        print(frames[frame_ind]['cells'][0][12])
-    print("\n")
     """
     
 def export_to_json(frames):
--- Plots.py
+++ Plots.py
@@ -8,6 +8,7 @@ def plot_cell(frames, row_ind, col_ind):
     times = []
     pred_labels = []
     new_labels = []
+    ages = []
 
     label_colors = {
               '(empty)': "green",
@@ -18,25 +19,33 @@ def plot_cell(frames, row_ind, col_ind):
               '(has bee head)':"cyan",
               '(unknown)':"red",
               '(has egg, has larva)':"indigo",
+              '(has egg, has old pupa)':"gold",
+              '(has egg, has bee head)':"olive",
+              '(has young pupa, has old pupa)':"darkred",
               '(has old pupa, has bee head)':"midnightblue"}
 
-    label_order = ['(empty)','(has egg)', '(has larva)', '(has young pupa)', '(has old pupa)','(has bee head)','(unknown)','(has egg, has larva)', '(has old pupa, has bee head)']
-    for frame_ind in range(len(frames)):
+    label_order = ['(empty)','(has egg)','(has larva)','(has young pupa)','(has old pupa)','(has bee head)','(unknown)',
+                   '(has egg, has larva)','(has egg, has old pupa)','(has egg, has bee head)','(has young pupa, has old pupa)','(has old pupa, has bee head)']
+    for frame_ind in range(2067):
     #for frame_ind in range(0, 1000):
         times.append(datetime.datetime.fromtimestamp(frames[frame_ind]['time']))
-        #print(f'frame_ind: {frame_ind}, label: ' + frames[frame_ind]['cells'][row_ind][col_ind]['new_label'])
-        print(f'frame_ind: {frame_ind}, label: ' + frames[frame_ind]['cells'][row_ind][col_ind]['pred_label'])
+        #print(f'frame_ind: {frame_ind}, label: ' + frames[frame_ind]['cells'][row_ind][col_ind]['new_label'] + ', age: ' + str(frames[frame_ind]['cells'][row_ind][col_ind]['age']))
+        print(f'frame_ind: {frame_ind}, label: ' + frames[frame_ind]['cells'][row_ind][col_ind]['pred_label'] + ', age: ' + str(frames[frame_ind]['cells'][row_ind][col_ind]['age']))
         pred_labels.append(frames[frame_ind]['cells'][row_ind][col_ind]['pred_label'])
         new_labels.append(frames[frame_ind]['cells'][row_ind][col_ind]['new_label'])
+        ages.append(round(frames[frame_ind]['cells'][row_ind][col_ind]['age'] / 60 / 60 / 24, 2))
 
     # Convert datetime timestamps to numeric values
     numeric_timestamps = mdates.date2num(times)
 
-    # Plotting
+
+
+    # ------------------------------------------------------ Plotting --------------------------------------------------------------------------------
     fig, ax = plt.subplots()
 
     label_var = pred_labels
     #label_var = new_labels
+    show_ages = True
 
     unique_labels = (set(label_var))
     ordered_unique_labels = sorted(unique_labels,
@@ -56,8 +65,7 @@ def plot_cell(frames, row_ind, col_ind):
     sorted_dict = dict(sorted(dict_all_labels.items(), key=lambda item: label_order.index(item[1])))
     # scatterplot always constructs in order of occurance of the underlying data (from bottom to top)
     for timestamp, label in sorted_dict.items():
-            ax.scatter(timestamp, label, c=unique_colors[label], s=2, marker='s')
-    
+        ax.scatter(x=timestamp, y=label, c=unique_colors[label], s=2, marker='s', zorder=1)
     ax.set_xlabel('Timestamps')
     ax.set_ylabel('Labels')
 
@@ -70,12 +78,33 @@ def plot_cell(frames, row_ind, col_ind):
     ax.set_yticks(y_ticks)
     ax.set_yticklabels(ordered_unique_labels)
 
+    if show_ages:
+        ax2 = ax.twinx()
+        ax2.plot(numeric_timestamps, ages, 'k', zorder=2)
+        ax2.set_ylabel('Age in days')
+
     # Show the plot
     plt.show()
+
 def main():
     frames = import_from_json(filename="full_dataset_with_ages.json")
     #plot_cell(frames, 7, 4)
-    plot_cell(frames, 8, 4)
+    #plot_cell(frames, 8, 4)
+    #plot_cell(frames, 10, 9)
+    #plot_cell(frames, 0, 12)
+    #plot_cell(frames, 11, 14)
+    #plot_cell(frames, 1, 1)
+    #plot_cell(frames, 15, 12)
+    #plot_cell(frames, 9, 7)
+    #plot_cell(frames, 6, 7)
+    plot_cell(frames, 8, 7)
+
+    # in these ones it's hard to decide what happens even if manually looking at it
+    #plot_cell(frames, 5, 7)
+    #plot_cell(frames, 7, 7)
+    
+
+
 
 if __name__ == "__main__":
     main()
