893ae6c3408ef374962650d6f62e5445435a5472
joslin <j.thomas@turbit.de>
2023-07-10 00:00:29 +0200

    Testing added

--- Neural_Network/NN_Training_Epoch_50.py
+++ Neural_Network/NN_Training_Epoch_50.py
@@ -175,6 +175,6 @@ predictions_numpy = predictions.numpy()
 
 predictions_list = predictions_numpy.tolist()
 predictions_folder = '/content/SWP-Brood-Cell-AgeSoSe23-FU/Predictions'
-export_to_json(filename=f"predictions_test",
+export_to_json(filename=f"predictions_test1",
                folder=predictions_folder,
                file=predictions_list)
