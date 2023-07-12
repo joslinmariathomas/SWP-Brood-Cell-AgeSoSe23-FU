eb841a2a98755a0e6fbf4317b7b706c3977a222d
joslin <j.thomas@turbit.de>
2023-07-12 17:39:50 +0200

    Gaussian Blur

---
+++ Neural_Network/Gaussian_Blur/Gaussian_Blur.py
@@ -0,0 +1,61 @@
+import torch
+import os
+import random
+import torchvision.transforms.functional as TF
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+
+def apply_gaussian_blur(image_tensor):
+    blur_amount, blur_radius = 2.0,20
+    central_blur_amount, central_blur_radius = 0.9,7
+    # Convert the tensor to a numpy array
+    image_np = image_tensor.permute(1, 2, 0).numpy()
+
+    # Create a circular mask for the surrounding region
+    h, w, _ = image_np.shape
+    center_x, center_y = w // 2, h // 2
+    y, x = np.ogrid[:h, :w]
+    mask = np.sqrt((x - center_x) ** 2 + (y - center_y) ** 2) > blur_radius
+
+    # Apply Gaussian blur to the surroundings
+    blurred_np = np.copy(image_np)
+    blurred_np[mask] = gaussian_filter(image_np, sigma=blur_amount)[mask]
+
+    # Create a circular mask for the central region
+    central_mask = np.sqrt((x - center_x) ** 2 + (y - center_y) ** 2) <= central_blur_radius
+
+    # Apply a lighter Gaussian blur to the central region
+    central_blurred_np = np.copy(blurred_np)
+    central_blurred_np[central_mask] = gaussian_filter(image_np, sigma=central_blur_amount)[central_mask]
+
+    # Convert the blurred numpy array back to a tensor
+    central_blurred_tensor = torch.from_numpy(central_blurred_np).permute(2, 0, 1)
+
+    return central_blurred_tensor
+
+
+def training_tensor_gaussian_blur_save():
+    training_tensor_folder =  '/content/SWP-Brood-Cell-AgeSoSe23-FU/trainig_tensor_data/tensors'
+    for file_name in os.listdir(training_tensor_folder):
+        training_tensor = torch.load(
+            f'{training_tensor_folder}/{file_name}')
+        num_of_tensors = training_tensor.shape[0]
+        appended_tensors = []
+        for i in range(num_of_tensors):
+            image_tensor = training_tensor[i, :, :]
+            central_blurred_tensor = apply_gaussian_blur(image_tensor)
+            appended_tensors.append(central_blurred_tensor)
+
+        appended_tensor = torch.cat(appended_tensors, dim=0)
+        print(appended_tensor.shape)
+
+
+if __name__ == "__main__":
+    training_tensor_gaussian_blur_save()
+
+
+
+
+
