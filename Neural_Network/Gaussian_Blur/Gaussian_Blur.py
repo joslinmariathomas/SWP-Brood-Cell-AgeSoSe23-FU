45b499214efa5963a19456f1bb95e87a5306cfe0
joslinmariathomas <joslinmariathomas94@gmail.com>
2023-07-12 16:29:50 +0000

    Gaussian_Blur

--- Neural_Network/Gaussian_Blur/Gaussian_Blur.py
+++ Neural_Network/Gaussian_Blur/Gaussian_Blur.py
@@ -37,7 +37,8 @@ def apply_gaussian_blur(image_tensor):
 
 
 def training_tensor_gaussian_blur_save():
-    training_tensor_folder =  '/content/SWP-Brood-Cell-AgeSoSe23-FU/trainig_tensor_data/tensors'
+    training_tensor_folder =  "/content/SWP-Brood-Cell-AgeSoSe23-FU/training_tensor_data/tensors/"
+    folder_to_save = "/content/SWP-Brood-Cell-AgeSoSe23-FU/Neural_Network/Gaussian_Blur/training_tensor_data/tensors"
     for file_name in os.listdir(training_tensor_folder):
         training_tensor = torch.load(
             f'{training_tensor_folder}/{file_name}')
@@ -47,13 +48,34 @@ def training_tensor_gaussian_blur_save():
             image_tensor = training_tensor[i, :, :]
             central_blurred_tensor = apply_gaussian_blur(image_tensor)
             appended_tensors.append(central_blurred_tensor)
+            
 
-        appended_tensor = torch.cat(appended_tensors, dim=0)
-        print(appended_tensor.shape)
+        appended_tensor = torch.cat([tensor.unsqueeze(0) for tensor in appended_tensors], dim=0)
+        torch.save(appended_tensor,
+               f'{folder_to_save}/{file_name}')
+
+
+def testing_tensor_gaussian_blur_save():
+  testing_tensor_folder = "/content/SWP-Brood-Cell-AgeSoSe23-FU/testing_tensor_data/tensors/"
+  folder_to_save = "/content/SWP-Brood-Cell-AgeSoSe23-FU/Neural_Network/Gaussian_Blur/testing_tensor_data/tensors"
+  for file_name in os.listdir(testing_tensor_folder):
+    testing_tensor = torch.load(
+    f'{testing_tensor_folder}/{file_name}')
+    num_of_tensors = testing_tensor.shape[0]
+    appended_tensors = []
+    for i in range(num_of_tensors):
+      image_tensor = testing_tensor[i, :, :]
+      central_blurred_tensor = apply_gaussian_blur(image_tensor)
+      appended_tensors.append(central_blurred_tensor)
+
+    appended_tensor = torch.cat([tensor.unsqueeze(0) for tensor in appended_tensors], dim=0)
+    torch.save(appended_tensor,
+    f'{folder_to_save}/{file_name}')
 
 
 if __name__ == "__main__":
     training_tensor_gaussian_blur_save()
+    testing_tensor_gaussian_blur_save()
 
 
 
--- Neural_Network/NN_tensor_concat_export_colab.py
+++ Neural_Network/NN_tensor_concat_export_colab.py
@@ -82,8 +82,8 @@ def save_tensor_and_ages(train_or_test: str, tensor_folder_path,
 
 
 def main():
-    training_data_folder = '/content/SWP-Brood-Cell-AgeSoSe23-FU/training_tensor_data/tensors/'
-    training_labels_folder = '/content/SWP-Brood-Cell-AgeSoSe23-FU/training_tensor_data/labels/'
+    training_data_folder = '/content/SWP-Brood-Cell-AgeSoSe23-FU/Neural_Network/Gaussian_Blur/training_tensor_data/tensors/'
+    training_labels_folder = '/content/SWP-Brood-Cell-AgeSoSe23-FU/Neural_Network/Gaussian_Blur/training_tensor_data/labels/'
     folder_to_save = '/content/SWP-Brood-Cell-AgeSoSe23-FU/Neural_Network'
     save_tensor_and_ages(train_or_test="train",
                          tensor_folder_path=training_data_folder,
