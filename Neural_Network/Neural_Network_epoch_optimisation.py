efab03dffd9a8b18f16e6e4bac0319b80266b314
joslin <j.thomas@turbit.de>
2023-07-13 00:41:12 +0200

    Initial model

--- Neural_Network/Neural_Network_epoch_optimisation.py
+++ Neural_Network/Neural_Network_epoch_optimisation.py
@@ -63,7 +63,7 @@ validation_data = (validation_data - data_mean) / data_std
 
 # Perform training iterations
 num_epochs = 100
-batch_size = 50  # Choose an appropriate batch size
+batch_size = 32  # Choose an appropriate batch size
 num_training_batches = (num_training - 1) // batch_size + 1
 num_validation_batches = (num_validation - 1) // batch_size + 1
 
