8d33bbc18f5d52a36a200b1b9b03146d9b5a292e
joslin <j.thomas@turbit.de>
2023-07-13 02:27:40 +0200

    Model 2

--- Neural_Network/Neural_Network_model.py
+++ Neural_Network/Neural_Network_model.py
@@ -6,26 +6,33 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import torch
 import torch.nn as nn
 
+
 class CellModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=5, stride=2, padding=1),
+            nn.Conv2d(3, 16, kernel_size=5, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=5, stride=2, padding=0),
+            nn.Conv2d(16, 16, kernel_size=5, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(16, 16, kernel_size=5, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=0),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(64, 1, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=0),
+              # Output layer with 1 channel
         )
 
     def forward(self, x):
         x = self.layers(x)
         x = torch.flatten(x, start_dim=1)
         return x
+
