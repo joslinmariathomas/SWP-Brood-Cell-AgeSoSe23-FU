f6d70d680c0c27864a5d1cd7a1e9a44ed5c4e7a6
joslin <j.thomas@turbit.de>
2023-07-15 17:16:32 +0200

    Resize tensor

--- Neural_Network/Image_Augmentation/Image_Augmentation.py
+++ Neural_Network/Image_Augmentation/Image_Augmentation.py
@@ -18,7 +18,6 @@ def augment_image(image_tensor):
     # Apply the selected augmentation technique to the image
     augmented_image = augmentation(image)
     resized_image = augmented_image.resize((64, 64))
-
     # Convert the resized PIL Image back to tensor
     resized_tensor = transforms.ToTensor()(resized_image)
     return resized_tensor
