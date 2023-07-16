3a1ae2b6d4584f9bd7dd3d00d53788b2303dab2f
joslin <j.thomas@turbit.de>
2023-07-16 14:11:23 +0200

    Grayscale modified

--- Neural_Network/Grayscale/Grayscale_Augmentation.py
+++ Neural_Network/Grayscale/Grayscale_Augmentation.py
@@ -1,8 +1,9 @@
 import random
 import torchvision.transforms as T
 
-def augment_image(image_cell,probability:float):
-    augmented_tensor =image_cell
+
+def augment_image(image_cell, probability: float):
+    augmented_tensor = image_cell
 
     if random.random() < probability:
         augmented_tensor = T.GaussianBlur(kernel_size=3, sigma=0.75)(
@@ -18,8 +19,6 @@ def augment_image(image_cell,probability:float):
     if random.random() < probability:
         augmented_tensor = T.RandomVerticalFlip()(augmented_tensor)
 
-    if random.random() < probability:
-        augmented_tensor = T.RandomGrayscale()(augmented_tensor)
-
+    augmented_tensor = T.Grayscale()(augmented_tensor)
 
     return augmented_tensor
