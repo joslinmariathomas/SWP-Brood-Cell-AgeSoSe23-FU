8e8381e95ab27cd8cb5b7f56a02c2bb9fb7d8967
joslin <j.thomas@turbit.de>
2023-07-16 18:35:29 +0200

    Grayscale and gaussian blur

--- Neural_Network/Grayscale/Grayscale_Augmentation.py
+++ Neural_Network/Grayscale/Grayscale_Augmentation.py
@@ -38,21 +38,21 @@ def apply_gaussian_blur(image_tensor):
 
 
 def augment_image(image_cell, probability: float):
-    augmented_tensor = apply_gaussian_blur(image_cell)
+    # augmented_tensor = apply_gaussian_blur(image_cell)
+    augmented_tensor = image_cell
+    if random.random() < probability:
+        augmented_tensor = T.GaussianBlur(kernel_size=3, sigma=0.75)(
+            augmented_tensor)
 
-    # if random.random() < probability:
-    #     augmented_tensor = T.GaussianBlur(kernel_size=3, sigma=0.75)(
-    #         augmented_tensor)
-    #
-    # if random.random() < probability:
-    #     augmented_tensor = T.RandomAdjustSharpness(sharpness_factor=2)(
-    #         augmented_tensor)
-    #
-    # if random.random() < probability:
-    #     augmented_tensor = T.RandomHorizontalFlip()(augmented_tensor)
-    #
-    # if random.random() < probability:
-    #     augmented_tensor = T.RandomVerticalFlip()(augmented_tensor)
+    if random.random() < probability:
+        augmented_tensor = T.RandomAdjustSharpness(sharpness_factor=2)(
+            augmented_tensor)
+
+    if random.random() < probability:
+        augmented_tensor = T.RandomHorizontalFlip()(augmented_tensor)
+
+    if random.random() < probability:
+        augmented_tensor = T.RandomVerticalFlip()(augmented_tensor)
 
     augmented_tensor = T.Grayscale()(augmented_tensor)
 
